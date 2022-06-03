a879e8672affa90e593abb2d13f8ee68bf3cef7b
ULTR5RM <ULTR.RM79@gmail.com>
2022-06-03 11:05:29 +0700

    commit

---
+++ app/src/main/java/com/example/pr11_tarasova_ulyana_pr_20101_5var/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pr11_tarasova_ulyana_pr_20101_5var
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
